81b93d273f1f5bba11e94b4990a887ab6b272b9c
joshuamackey <156105031+joshuamackey@users.noreply.github.com>
2024-10-27 09:52:03 -0500

    First one.

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <h1>Vue.js on Azure App Services</h1>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor itaque 
+      perferendis cum, cumque velit nesciunt. Nihil, animi facere minus hic 
+      tempora quaerat voluptates? Unde iusto minima libero, pariatur totam error?
+    </p>
+  </div>
+</template>
